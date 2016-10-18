2b0a528a9465bb84e5a1f7158a5fed01965c38e0
Rodolfo Díaz <rodolfodc@hotmail.com>
2016-10-18 22:47:46 +0000

    Updated configuration files

--- lib/angularjs/angular-seed/test.sh
+++ lib/angularjs/angular-seed/test.sh
@@ -1,8 +1,11 @@
+# Configure Karma conf with PhantomJS to run in Cloud9
 cd angular-seed
-# mv karma.conf.js karma.conf.js.bak
-# In '/angular-seed/karma.conf.js' 
-# 1- inside browsers section, change: ['Chrome'] to be: browsers: ['PhantomJS']
-# 2- inside plugins section, add the following: 'karma-phantomjs-launcher',
-npm install karma-phantomjs-launcher --save-dev
+cp karma.conf.js karma.conf.js.bak
+# Inside '/angular-seed/karma.conf.js' file:
+# 1- inside browsers section, change: ['Chrome'] to be: ['PhantomJS']
+sed -i -e 's/Chrome/PhantomJS/g' karma.conf.js
+# 2- inside plugins section, change: 'karma-chrome-launcher' to be 'karma-phantomjs-launcher',
+sed -i -e 's/karma-chrome-launcher/karma-phantomjs-launcher/g' karma.conf.js
 npm install phantomjs-prebuilt --save-dev
+npm install karma-phantomjs-launcher --save-dev
 npm test
