cf08d9679d011d16d93cdd7ff3cc8e8f1e6a3325
Rodolfo Díaz <rodolfodc@hotmail.com>
2016-10-25 18:19:26 +0000

    Added code sample

--- gulp/angular-seed-gulp/README.md
+++ gulp/angular-seed-gulp/README.md
@@ -1,5 +1,5 @@
-* Gulp.js
+# Gulp.js
 
-Gulp code sample
+Gulp code sample based on Angular Seed repo
 
 More information about Gulp.js is available [here](http://gulpjs.com/).
---
+++ gulp/angular-seed-gulp/angular-seed/gulpfile.js
@@ -0,0 +1,12 @@
+// gulp
+var gulp = require('gulp');
+
+// plugins
+var connect = require('gulp-connect');
+
+gulp.task('connect', function () {
+  connect.server({
+    root: 'app/',
+    port: 8080
+  });
+});
