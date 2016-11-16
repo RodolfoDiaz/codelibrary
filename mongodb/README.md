5479fe143b4922dfdb67539201313add80780434
Rodolfo Díaz <rodolfodc@hotmail.com>
2016-11-16 21:35:43 +0000

    MongoDB information

---
+++ mongodb/README.md
@@ -0,0 +1,5 @@
+# MongoDB
+
+MongoDB is a scalable, high-performance, open source NoSQL database. https://docs.mongodb.com/manual/
+* [Setting Up MongoDB](https://community.c9.io/t/setting-up-mongodb/1717)
+* [Introduction to Cloud9 and MongoDB](https://c9.io/blog/introduction-to-cloud9-and-mongodb/)
--- mongodb/setup.sh
+++ mongodb/setup.sh
@@ -1,7 +1,8 @@
-echo "Installing MongoDB - https://community.c9.io/t/setting-up-mongodb/1717"
+echo "Installing MongoDB"
 sudo apt-get install -y mongodb-org
 echo "Running MongoDB on a Cloud9 workspace"
 mkdir data
 echo 'mongod --bind_ip=$IP --dbpath=data --nojournal --rest "$@"' > mongod
 chmod a+x mongod
 echo "You can start mongodb by running the mongod script"
+echo "To access a shell prompt for the above MongoDB run the following.   $ mongo"
