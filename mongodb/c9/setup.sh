24478c018f5a298005bb5e53d96354005a453529
Rodolfo Díaz <rodolfodc@hotmail.com>
2016-11-17 05:25:43 +0000

    Changes to the setup command.  Added new command file with examples

--- mongodb/c9/setup.sh
+++ mongodb/c9/setup.sh
@@ -1,5 +1,11 @@
 echo "Installing MongoDB"
+echo "Instructions installing on Ubuntu: https://docs.mongodb.com/manual/tutorial/install-mongodb-on-ubuntu/"
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+echo "Run command: lsb_release -a to check Ubuntu version.  For Ubuntu 14.04 you use the following:"
+echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+sudo apt-get update
 sudo apt-get install -y mongodb-org
+echo "Default configuration file is stored at: /etc/mongod.conf"
 echo "Running MongoDB on a Cloud9 workspace"
 mkdir data
 echo 'mongod --bind_ip=$IP --dbpath=data --nojournal --rest "$@"' > mongod
