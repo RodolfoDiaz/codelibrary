faba37abef78e4648e9d120112469ebd4e5c070b
Rodolfo Díaz <rodolfodc@hotmail.com>
2016-11-17 17:15:44 +0000

    Changes to the setup instructions

--- mongodb/c9/README.md
+++ mongodb/c9/README.md
@@ -1,5 +1,7 @@
 # MongoDB
 
 MongoDB is a scalable, high-performance, open source NoSQL database. https://docs.mongodb.com/manual/
+
 * [Setting Up MongoDB](https://community.c9.io/t/setting-up-mongodb/1717)
-* [Introduction to Cloud9 and MongoDB](https://c9.io/blog/introduction-to-cloud9-and-mongodb/)
+* [Install MongoDB Community Edition on Ubuntu](https://docs.mongodb.com/manual/tutorial/install-mongodb-on-ubuntu/)
+* [Install MongoDB Community Edition on OS X](https://docs.mongodb.com/manual/tutorial/install-mongodb-on-os-x/)
--- mongodb/c9/setup.sh
+++ mongodb/c9/setup.sh
@@ -1,14 +1,17 @@
-echo "Installing MongoDB"
-echo "Instructions installing on Ubuntu: https://docs.mongodb.com/manual/tutorial/install-mongodb-on-ubuntu/"
+# Install MongoDB Community Edition on Ubuntu
+# https://docs.mongodb.com/manual/tutorial/install-mongodb-on-ubuntu/
+# 1- Import the public key used by the package management system
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
-echo "Run command: lsb_release -a to check Ubuntu version.  For Ubuntu 14.04 you use the following:"
+# 2- Create a list file for MongoDB.
+# Run command: lsb_release -a to check Ubuntu version. For Ubuntu 14.04 you use the following:
 echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+# 3- Reload local package database.
 sudo apt-get update
+# 4- Install the MongoDB packages.
 sudo apt-get install -y mongodb-org
-echo "Default configuration file is stored at: /etc/mongod.conf"
-echo "Running MongoDB on a Cloud9 workspace"
+# Default configuration file is stored at: /etc/mongod.conf
+# 5- Create a 'mongod' (mongo daemon) script file for Cloud9 environment.
 mkdir data
-echo 'mongod --bind_ip=$IP --dbpath=data --nojournal --rest "$@"' > mongod
+echo 'mongod --bind_ip=$IP --dbpath=data --nojournal' > mongod
 chmod a+x mongod
-echo "You can start mongodb by running the mongod script"
-echo "To access a shell prompt for the above MongoDB run the following.   $ mongo"
+# You can start MongoDB server by running the $ ./mongod script, to access a shell prompt use: $ mongo
