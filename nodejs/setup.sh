c916b3602529359f38025c8902f9cfc0052a3ef9
Rodolfo Díaz <rodolfodc@hotmail.com>
2016-11-18 05:31:11 +0000

    Updated setup script for Node.js

---
+++ nodejs/setup.sh
@@ -0,0 +1,6 @@
+# Install Node Version Manager  
+# https://github.com/creationix/nvm
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.1/install.sh | bash
+# Execute the following command to install Node.js
+source ~/.nvm/nvm.sh
+nvm install 7 && nvm alias default 7
