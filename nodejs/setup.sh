a5c5b79afe85ef6fa9aefa7b51498be641824ba9
Rodolfo Díaz <rodolfodc@hotmail.com>
2016-11-18 16:38:52 +0000

    Changes to the setup script

--- nodejs/setup.sh
+++ nodejs/setup.sh
@@ -1,6 +1,6 @@
-# Install Node Version Manager  
+# Install Node Version Manager (Linux)
 # https://github.com/creationix/nvm
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.1/install.sh | bash
-# Execute the following command to install Node.js
+if ! [ -d ~/.nvm/ ]; then curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.1/install.sh | bash; fi
+# Install Node.js and select version 7 as default
 source ~/.nvm/nvm.sh
 nvm install 7 && nvm alias default 7
