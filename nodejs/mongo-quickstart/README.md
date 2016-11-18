5a23205005ef5a56ce65d29e5687e3445eda6c62
Rodolfo Díaz <rodolfodc@hotmail.com>
2016-11-18 21:57:00 +0000

    Added quickstart example from MongoDB nodeJS driver

---
+++ nodejs/mongo-quickstart/README.md
@@ -0,0 +1,8 @@
+# MongoDB Node.JS Driver
+
+Tests and examples from MongoDB Docs.
+See below the resources:
+
+* [MongoDB Node.JS Driver](https://mongodb.github.io/node-mongodb-native/)
+* [Quick Start](http://mongodb.github.io/node-mongodb-native/2.2/quick-start/)
+* [Tutorials](http://mongodb.github.io/node-mongodb-native/2.2/tutorials/)
---
+++ nodejs/mongo-quickstart/app.js
@@ -0,0 +1,93 @@
+var MongoClient = require('mongodb').MongoClient
+  , assert = require('assert');
+
+// Connection URL
+var url = 'mongodb://localhost:27017/myproject';
+
+// Use connect method to connect to the server
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+
+    findDocuments(db, function() {
+      db.close();
+    });
+    
+//   insertDocuments(db, function() {
+//       updateDocument(db, function() {
+//           removeDocument(db, function() {
+//               db.close();
+//           });
+//       });
+//   });
+
+    // insertDocuments(db, function() {
+    //     indexCollection(db, function() {
+    //         db.close();
+    //     });
+    // });
+  
+});
+
+var insertDocuments = function(db, callback) {
+  // Get the documents collection
+  var collection = db.collection('documents');
+  // Insert some documents
+  collection.insertMany([
+    {a : 1}, {a : 2}, {a : 3}
+  ], function(err, result) {
+    assert.equal(err, null);
+    assert.equal(3, result.result.n);
+    assert.equal(3, result.ops.length);
+    console.log("Inserted 3 documents into the collection");
+    callback(result);
+  });
+};
+
+var findDocuments = function(db, callback) {
+  // Get the documents collection
+  var collection = db.collection('documents');
+  // Find some documents
+  collection.find({'a': 3}).toArray(function(err, docs) {
+    assert.equal(err, null);
+    console.log("Found the following records");
+    console.log(docs);
+    callback(docs);
+  });
+};
+
+var updateDocument = function(db, callback) {
+  // Get the documents collection
+  var collection = db.collection('documents');
+  // Update document where a is 2, set b equal to 1
+  collection.updateOne({ a : 2 }
+    , { $set: { b : 1 } }, function(err, result) {
+    assert.equal(err, null);
+    assert.equal(1, result.result.n);
+    console.log("Updated the document with the field a equal to 2");
+    callback(result);
+  });  
+};
+
+var removeDocument = function(db, callback) {
+  // Get the documents collection
+  var collection = db.collection('documents');
+  // Insert some documents
+  collection.deleteOne({ a : 3 }, function(err, result) {
+    assert.equal(err, null);
+    assert.equal(1, result.result.n);
+    console.log("Removed the document with the field a equal to 3");
+    callback(result);
+  });    
+};
+
+var indexCollection = function(db, callback) {
+  db.collection('documents').createIndex(
+    { "a": 1 },
+      null,
+      function(err, results) {
+        console.log(results);
+        callback();
+    }
+  );
+};
