854356bfea3b4ad1da8214fddb48e8da90e6aea7
Rodolfo Díaz <rodolfodc@hotmail.com>
2017-01-18 16:24:43 +0000

    Added Python examples

---
+++ python/examples/AddTwoNumbers.py
@@ -0,0 +1,11 @@
+# This program adds two numbers
+# https://www.programiz.com/python-programming/examples/add-number
+
+num1 = 1.5
+num2 = 6.3
+
+# Add two numbers
+sum = float(num1) + float(num2)
+
+# Display the sum
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
---
+++ python/examples/README.md
@@ -0,0 +1,7 @@
+# Python Programming Examples
+
+For reference:
+
+* [Examples](https://www.programiz.com/python-programming/examples)
+
+About Markdown see the [cheatsheet](https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet)
