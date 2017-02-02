af3970741448a617a9706f5a6d52a3a814b98ed2
Rodolfo Díaz <rodolfodc@hotmail.com>
2017-02-02 17:39:12 +0000

    Added setup configuration file

--- python/django/setup.sh
+++ python/django/setup.sh
@@ -1,5 +1,6 @@
-#apt-get update
-#apt-get -y install python-pip
+#sudo apt-get update
+#sudo apt-get -y install python
+#sudo apt-get -y install python-pip
 sudo -H pip install -U pip 
 sudo -H pip install django --upgrade
 echo "Django version is: "
