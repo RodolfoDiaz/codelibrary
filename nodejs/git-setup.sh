507190849ce2406a9a895b5d4cb02f0376b6bbcb
RodolfoDiaz <rodolfodc@hotmail.com>
2017-02-04 19:26:09 -0600

    Configuration file

---
+++ nodejs/git-setup.sh
@@ -0,0 +1,6 @@
+#Install Git (Ubuntu Linux)
+#https://git-scm.com/download/linux
+sudo apt-get install git
+git config --global user.email "rodolfodc@hotmail.com"
+git config --global user.name "RodolfoDiaz"
+git config --list
--- nodejs/setup.sh
+++ nodejs/setup.sh
@@ -1,6 +1,8 @@
 # Install Node Version Manager (Linux)
 # https://github.com/creationix/nvm
-if ! [ -d ~/.nvm/ ]; then curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash; fi
+if ! [ -d ~/.nvm/ ]; then
+  curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash;
+fi
 # Install Node.js and select version 7 as default
 source ~/.nvm/nvm.sh
 nvm install 7 && nvm alias default 7
