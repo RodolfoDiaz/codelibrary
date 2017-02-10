9a68de9d3fbc4daf8358596aa3a3053a9610b0bd
Rodolfo Díaz <rodolfodc@hotmail.com>
2017-02-10 17:09:21 +0000

    Added a Hello World example

---
+++ nodejs/hello/hello-html.js
@@ -0,0 +1,14 @@
+var http = require('http');
+
+const hostname = process.env.IP || "0.0.0.0";
+const port = process.env.PORT || 3000;
+
+var server = http.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type':'text/html'});
+    res.write('<html><body><h1 style="color:green;">Hello World!</h1></body></html>');
+    res.end();
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
