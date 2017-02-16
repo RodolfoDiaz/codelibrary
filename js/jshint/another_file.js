8a959fed2be445bc55b7e446e1a1d68d9ecc342b
Rodolfo Díaz <rodolfodc@hotmail.com>
2017-02-16 17:32:58 +0000

    Added code sample with JSHint ignore file

---
+++ js/jshint/another_file.js
@@ -0,0 +1,10 @@
+// if you run jshint, it will find an error in the line below because
+// 'const' is only available in ES6. The .jshintignore is used as example.
+const pi = 3.1415;
+
+function main() {
+    'use strict';
+    console.log('The value of Pi is ' + pi);
+}
+
+main();
