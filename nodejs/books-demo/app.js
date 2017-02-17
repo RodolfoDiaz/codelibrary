19b410a9018605b2c76c782f86f48cc53e4d59f0
RodolfoDiaz <rodolfodc@hotmail.com>
2017-02-17 11:13:33 -0600

    Added API

--- nodejs/books-demo/app.js
+++ nodejs/books-demo/app.js
@@ -5,11 +5,23 @@ var app = express();
 const hostname = process.env.IP || "0.0.0.0";
 const port = process.env.PORT || 3000;
 
-app.get('/', function(req, res){
-  res.send('Welcome to my API :)');
+var bookRouter = express.Router();
+
+bookRouter.route('/books')
+    .get(function(req, res) {
+        var responseJson = {
+            hello: "This is my API"
+        };
+        res.json(responseJson);
+    });
+
+app.use('/api', bookRouter);
+
+app.get('/', function(req, res) {
+    res.send('Welcome to my API :)');
 });
 
-app.listen(port, hostname, function(){
-  console.log('Listening on ' + hostname + ' and port: ' + port);
-  // you should be able to browse to http://localhost:{PORT}
+app.listen(port, hostname, function() {
+    console.log('Listening on ' + hostname + ' and port: ' + port);
+    // you should be able to browse to http://localhost:{PORT}
 });
---
+++ nodejs/books-demo/setup.sh
@@ -0,0 +1,2 @@
+if ! [ -d ./node_modules/ ]; then echo Installing Dependencies; npm install; fi
+npm install gulp -g
