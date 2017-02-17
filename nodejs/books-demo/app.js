0a5499a232825b57393d664532f10e20055a25fa
RodolfoDiaz <rodolfodc@hotmail.com>
2017-02-17 13:02:36 -0600

    Added database integration through MongoDB

--- nodejs/books-demo/app.js
+++ nodejs/books-demo/app.js
@@ -1,4 +1,9 @@
 var express = require('express');
+var mongoose = require('mongoose');
+
+var db = mongoose.connect('mongodb://localhost/bookAPI');
+
+var Book = require('./models/bookModel');
 
 var app = express();
 
@@ -9,10 +14,13 @@ var bookRouter = express.Router();
 
 bookRouter.route('/books')
     .get(function(req, res) {
-        var responseJson = {
-            hello: "This is my API"
-        };
-        res.json(responseJson);
+        Book.find(function(err, books) {
+            if (err)
+                res.status(500).send(err);
+            else {
+                res.json(books);
+            }
+        });
     });
 
 app.use('/api', bookRouter);
