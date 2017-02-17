0bb4c386562f5ede40ab6387fbd0870bfda2d06c
RodolfoDiaz <rodolfodc@hotmail.com>
2017-02-16 20:08:40 -0600

    Added Node and Express code sample.

---
+++ nodejs/books-demo/app.js
@@ -0,0 +1,15 @@
+var express = require('express');
+
+var app = express();
+
+const hostname = process.env.IP || "0.0.0.0";
+const port = process.env.PORT || 3000;
+
+app.get('/', function(req, res){
+  res.send('Welcome to my API');
+});
+
+app.listen(port, hostname, function(){
+  console.log('Listening on ' + hostname + ' and port: ' + port);
+  // you should be able to browse to http://localhost:3000
+});
