0d7c0daea32385b3d8ba98f45314a30fd886f8da
RodolfoDiaz <rodolfodc@hotmail.com>
2017-02-17 10:08:15 -0600

    Getting gulp set up

--- nodejs/books-demo/app.js
+++ nodejs/books-demo/app.js
@@ -6,10 +6,10 @@ const hostname = process.env.IP || "0.0.0.0";
 const port = process.env.PORT || 3000;
 
 app.get('/', function(req, res){
-  res.send('Welcome to my API');
+  res.send('Welcome to my API :)');
 });
 
 app.listen(port, hostname, function(){
   console.log('Listening on ' + hostname + ' and port: ' + port);
-  // you should be able to browse to http://localhost:3000
+  // you should be able to browse to http://localhost:{PORT}
 });
