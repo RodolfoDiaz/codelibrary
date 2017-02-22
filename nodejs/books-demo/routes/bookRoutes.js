851819c176924a748317153e77667ac8cea69b49
RodolfoDiaz <rodolfodc@hotmail.com>
2017-02-22 12:34:12 -0600

    Implemented PATCH method.

--- nodejs/books-demo/routes/bookRoutes.js
+++ nodejs/books-demo/routes/bookRoutes.js
@@ -53,8 +53,30 @@
                 req.book.author = req.body.author;
                 req.book.genre = req.body.genre;
                 req.book.read = req.body.read;
-                req.book.save();
-                res.json(req.book);
+                req.book.save(function(err) {
+                    if (err) {
+                        res.status(500).send(err);
+                    } else {
+                        res.json(req.book);
+                    }
+                });
+            })
+            .patch(function(req, res) {
+                if (req.body._id) {
+                    delete req.body._id;
+                }
+                for (var key in req.body) {
+                    if (req.body.hasOwnProperty(key)) {
+                        req.book[key] = req.body[key];
+                    }
+                }
+                req.book.save(function(err) {
+                    if (err) {
+                        res.status(500).send(err);
+                    } else {
+                        res.json(req.book);
+                    }
+                });
             });
 
         return bookRouter;
