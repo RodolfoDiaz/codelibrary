2001bdd68a09be8c865e61efe96296f8b2f00681
RodolfoDiaz <rodolfodc@hotmail.com>
2017-02-22 17:49:41 -0600

    Added setup file.

---
+++ info/atom_setup.sh
@@ -0,0 +1,33 @@
+brew update
+brew install python
+brew install python3
+brew install node
+echo "---INSTALL ATOM PACKAGE---"
+apm install atom-beautify
+brew install uncrustify
+pip install jsbeautifier
+pip install autopep8
+echo "---INSTALL ATOM PACKAGE---"
+apm install atom-easy-jsdoc
+echo "---INSTALL ATOM PACKAGE---"
+apm install file-icons
+echo "---INSTALL ATOM PACKAGE---"
+apm install git-plus
+echo "---INSTALL ATOM PACKAGE---"
+apm install linter
+echo "---INSTALL ATOM PACKAGE---"
+apm install linter-clang
+echo "---INSTALL ATOM PACKAGE---"
+apm install linter-eslint
+npm install -g eslint
+npm install -g eslint-plugin-angular
+echo "---INSTALL ATOM PACKAGE---"
+apm install linter-htmlhint
+echo "---INSTALL ATOM PACKAGE---"
+apm install linter-jshint
+npm install -g jshint
+echo "---INSTALL ATOM PACKAGE---"
+apm install linter-pylama
+pip install pylama-pylint
+echo "---INSTALL ATOM PACKAGE---"
+apm install minimap
