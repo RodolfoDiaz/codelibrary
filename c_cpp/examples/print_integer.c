372fe51af151bd5939d786a948bbd36ea6efd0bf
RodolfoDiaz <rodolfodc@hotmail.com>
2017-02-21 22:36:16 -0600

    Renamed folder cpp => c_cpp

---
+++ c_cpp/examples/print_integer.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+        // https://www.programiz.com/c-programming/examples
+        int number;
+
+        // printf() dislpays the formatted output
+        printf("Enter an integer: ");
+
+        // scanf() reads the formatted input and stores them
+        scanf("%d", &number);
+
+        // printf() displays the formatted output
+        printf("You entered: %d\n", number);
+        return 0;
+}
