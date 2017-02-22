b2ded0917c2cb68db0a4512d0fd04d623a147ad3
RodolfoDiaz <rodolfodc@hotmail.com>
2017-02-22 12:21:58 -0600

    Added middle-ware function use to handle GET by ID

--- nodejs/books-demo/routes/bookRoutes.js
+++ nodejs/books-demo/routes/bookRoutes.js
@@ -30,30 +30,31 @@
                 });
             });
 
+        bookRouter.use('/:bookId', function(req, res, next) {
+            Book.findById(req.params.bookId, function(err, book) {
+                if (err) {
+                    res.status(500).send(err);
+                } else if (book) {
+                    req.book = book;
+                    next();
+                } else {
+                    res.status(404).send('no book found!');
+                }
+            });
+        });
+
         bookRouter.route('/:bookId')
             //support a query by ID, for example: http://localhost:8080/api/books/58a747202ba0ef40e1188b2b
             .get(function(req, res) {
-                Book.findById(req.params.bookId, function(err, book) {
-                    if (err) {
-                        res.status(500).send(err);
-                    } else {
-                        res.json(book);
-                    }
-                });
+                res.json(req.book);
             })
             .put(function(req, res) {
-                Book.findById(req.params.bookId, function(err, book) {
-                    if (err) {
-                        res.status(500).send(err);
-                    } else {
-                        book.title = req.body.title;
-                        book.author = req.body.author;
-                        book.genre = req.body.genre;
-                        book.read = req.body.read;
-                        book.save();
-                        res.json(book);
-                    }
-                });
+                req.book.title = req.body.title;
+                req.book.author = req.body.author;
+                req.book.genre = req.body.genre;
+                req.book.read = req.body.read;
+                req.book.save();
+                res.json(req.book);
             });
 
         return bookRouter;
