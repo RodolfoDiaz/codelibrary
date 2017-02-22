ae924d284bef92faa217fbdfaed047cb696effd9
RodolfoDiaz <rodolfodc@hotmail.com>
2017-02-22 15:27:49 -0600

    Added unit testing code.

--- nodejs/books-demo/controllers/bookController.js
+++ nodejs/books-demo/controllers/bookController.js
@@ -11,8 +11,15 @@
          */
         var post = function(req, res) {
             var book = new Book(req.body);
-            book.save();
-            res.status(201).send(book);
+
+            if (!req.body.title) {
+                res.status(400);
+                res.send('Title is required');
+            } else {
+                book.save();
+                res.status(201);
+                res.send(book);
+            }
         };
 
         /**
--- nodejs/books-demo/gulpfile.js
+++ nodejs/books-demo/gulpfile.js
@@ -2,19 +2,29 @@
     'use strict';
     var gulp = require('gulp');
     var nodemon = require('gulp-nodemon');
+    var gulpMocha = require('gulp-mocha');
 
     gulp.task('default', function() {
         nodemon({
-            script: 'app.js',
-            ext: 'js',
-            env: {
-                PORT: 8080
-            },
-            ignore: ['./node_modules/**']
-        })
-        .on('restart', function() {
-            console.log('Restarting...');
-        });
+                script: 'app.js',
+                ext: 'js',
+                env: {
+                    PORT: 8080
+                },
+                ignore: ['./node_modules/**']
+            })
+            .on('restart', function() {
+                console.log('Restarting...');
+            });
+    });
+
+    gulp.task('test', function() {
+        gulp.src('tests/*.js', {
+                read: false
+            })
+            .pipe(gulpMocha({
+                reporter: 'nyan'
+            }))
     });
 
 }());
