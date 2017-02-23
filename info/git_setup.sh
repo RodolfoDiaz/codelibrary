225a75e0628f610d8465a7cffcbbc764c0976c10
Rodolfo Díaz <rodolfodc@hotmail.com>
2017-02-23 19:24:16 +0000

    Moved file

--- info/git_setup.sh
+++ info/git_setup.sh
@@ -1,5 +1,5 @@
-#Install Git (Ubuntu Linux)
-#https://git-scm.com/download/linux
+# Install Git
+# https://git-scm.com/download/linux
 sudo apt-get install git
 git config --global user.email "rodolfodc@hotmail.com"
 git config --global user.name "RodolfoDiaz"
