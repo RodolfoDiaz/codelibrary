0e6a139d9b19e9f1daa2916a01380c34efba004d
Rodolfo Díaz <rodolfodc@hotmail.com>
2017-02-23 19:18:26 +0000

    Added a configuration setting.

--- nodejs/git-setup.sh
+++ nodejs/git-setup.sh
@@ -3,4 +3,5 @@
 sudo apt-get install git
 git config --global user.email "rodolfodc@hotmail.com"
 git config --global user.name "RodolfoDiaz"
+git config --global core.editor "nano"
 git config --list
