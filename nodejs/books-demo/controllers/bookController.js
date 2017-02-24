71005f941dd4b7229d2274e98a4ce3c25e21472f
RodolfoDiaz <rodolfodc@hotmail.com>
2017-02-23 18:11:52 -0600

    Applied code formatting.

--- nodejs/books-demo/controllers/bookController.js
+++ nodejs/books-demo/controllers/bookController.js
@@ -15,8 +15,7 @@
             if (!req.body.title) {
                 res.status(400);
                 res.send('Title is required');
-            }
-            else {
+            } else {
                 book.save();
                 res.status(201);
                 res.send(book);
@@ -43,8 +42,7 @@
             Book.find(query, function(err, books) {
                 if (err) {
                     res.status(500).send(err);
-                }
-                else {
+                } else {
                     var returnBooks = [];
                     books.forEach(function(element, index, array) {
                         var newBook = element.toJSON();
