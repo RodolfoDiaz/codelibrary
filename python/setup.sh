245c19b98c8674929e23030253d4393da468fe9b
RodolfoDiaz <rodolfodc@hotmail.com>
2017-05-04 21:08:57 +0000

    Changed install command

--- python/setup.sh
+++ python/setup.sh
@@ -5,6 +5,6 @@ if [ "$(uname)" == "Darwin" ]; then
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     echo "Install Python in Linux"
     sudo apt-get update
-    sudo apt-get install python2.7 -y
-    sudo apt-get install python3.6 -y
+    sudo apt-get install python -y
+    sudo apt-get install python3 -y
 fi
