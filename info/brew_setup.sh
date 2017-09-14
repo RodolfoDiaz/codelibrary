15d1b192cb3c1f3f1a8ce7ae482d845e03a66051
RodolfoDiaz <rodolfodc@hotmail.com>
2017-09-13 20:59:06 -0600

    Changed script with command-line arguments

--- info/brew_setup.sh
+++ info/brew_setup.sh
@@ -7,3 +7,30 @@ else
   echo "Installing Homebrew..."
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
+
+# Uninstall Homebrew applications
+if [ "$1" == "u" ]; then
+   brew uninstall git
+   brew uninstall node
+   brew uninstall python
+   brew uninstall python3
+   brew uninstall unrar
+   # uninstalling dependencies for node
+   brew uninstall icu4c
+   # uninstalling dependecies for python
+   brew uninstall sqlite
+   brew uninstall gdbm
+   brew uninstall openssl
+   brew uninstall readline
+   # uninstalling dependencies for python3
+   brew uninstall xz
+fi
+
+# Install Homebrew applications
+if [ "$1" == "i" ]; then
+   brew install git
+   brew install node
+   brew install python
+   brew install python3
+   brew install unrar
+fi
