c53a4368b5b9b52babcaddeba32951330926d0b6
RodolfoDiaz <rodolfodc@hotmail.com>
2017-09-13 22:12:14 -0600

    Added command to install pip

--- python/setup.sh
+++ python/setup.sh
@@ -1,7 +1,8 @@
 if [ "$(uname)" == "Darwin" ]; then
-    echo "Install Python in macOS"
+    echo "Install Python and pip in macOS"
     brew install python
     brew install python3
+    sudo easy_install pip
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     echo "Install Python in Linux"
     sudo apt-get update
