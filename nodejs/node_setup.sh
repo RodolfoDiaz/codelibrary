b8d17c046d042dd61638f6f7a191f41b0b1fcf6b
Rodolfo Díaz <rodolfodc@hotmail.com>
2018-01-17 06:17:33 +0000

    Updated Node.js version setting

--- nodejs/node_setup.sh
+++ nodejs/node_setup.sh
@@ -6,9 +6,9 @@ if [ "$(uname)" == "Darwin" ]; then
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     # Install Node Version Manager - https://github.com/creationix/nvm
     if ! [ -d ~/.nvm/ ]; then
-      curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.4/install.sh | bash
+      curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
     fi
-    # Install Node.js and select version 8 as default
+    # Install Node.js and select the latest version as default
     source ~/.nvm/nvm.sh
-    nvm install 8 && nvm alias default 8
+    nvm install 9 && nvm alias default 9
 fi
