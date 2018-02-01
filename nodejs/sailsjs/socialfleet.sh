dfa84a094cdc53af5073b95dc2b89517e0ce2692
Rodolfo Díaz <rodolfodc@hotmail.com>
2018-02-01 21:05:31 +0000

    Changed demo file

--- nodejs/sailsjs/socialfleet.sh
+++ nodejs/sailsjs/socialfleet.sh
@@ -1,9 +1,11 @@
 # Based on Pluralsight course
 # https://app.pluralsight.com/library/courses/two-tier-enterprise-app-api-development-angular-sails/
+if [ -d ./socialfleet/ ]; then rm -rf socialfleet; fi
 npm install sails -g
 mkdir socialfleet
 cd socialfleet
-sails new
+sails new backend
+cd backend
 sails lift
 # browse to http://localhost:1337
 # Press key CTRL-C to exit Sails process
@@ -12,7 +14,7 @@ sails lift
 # browse to http://localhost:1337/user
 # --> now, let's create a new user:
 # browse to http://localhost:1337/user/create?name=Rodolfo
-# browse to http://localhost:1337/user/create?name=Jose&email=jose@mail.com
+# browse to http://localhost:1337/user/create?name=Rodolfo%20Díaz&email=rodolfo@mail.com
 # --> Get all users created
 # browse to http://localhost:1337/user
 # --> Find certain user
