dce14dbb92c96dae12f493fedf98b0f062084b75
Rodolfo Díaz <rodolfodc@hotmail.com>
2018-08-08 23:52:20 +0000

    Changed code to run application

--- angular/basic-spa/run-app.sh
+++ angular/basic-spa/run-app.sh
@@ -1,7 +1,13 @@
+which ng
+if [ $? == 1 ]; then
+    echo "Installing Angular CLI"
+    npm install -g @angular/cli@latest
+fi
 if ! [ ${IP+x} ]; then export IP=localhost; fi
 if ! [ ${PORT+x} ]; then export PORT=8080; fi
 echo "IP is:" $IP " and PORT is:" $PORT
 cd route-test
+if ! [ -d ./node_modules/ ]; then echo Installing Dependencies; npm install; fi
 ng serve --open --host $IP --port $PORT --disable-host-check
 # The "--disable-host-check" flag is used as workaround if your execute from Cloud9,
 # to disable the validation of allowed hosts.  Avoid this flag if possible.
