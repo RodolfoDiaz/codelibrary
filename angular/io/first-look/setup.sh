c1a0084abd6dfdefd3d0d9bca9cb747359057a15
Rodolfo Díaz <rodolfodc@hotmail.com>
2018-09-25 20:39:19 +0000

    Fixed issue with C9

--- angular/io/first-look/setup.sh
+++ angular/io/first-look/setup.sh
@@ -1,6 +1,7 @@
+if ! [ ${IP+x} ]; then export IP=localhost; fi
+if ! [ ${PORT+x} ]; then export PORT=8080; fi
 rm -rf ./angular-first-look-hosted
 git clone https://github.com/johnpapa/angular-first-look-hosted.git
-cp bs-config.json ./angular-first-look-hosted
 cd angular-first-look-hosted
 npm install
-npm start
+ng serve --open --host $IP --port $PORT --disable-host-check
