e3917e3685a736a8af013b0bff8921e79ac5f05e
Rodolfo Díaz <rodolfodc@hotmail.com>
2018-09-25 19:38:00 +0000

    Angular code samples

--- angular/a2/a2-first-look/setup.sh
+++
@@ -1,5 +0,0 @@
-git clone https://github.com/johnpapa/angular2-first-look.git
-cp bs-config.json ./angular2-first-look
-cd angular2-first-look
-npm install
-npm start
---
+++ angular/io/first-look/setup.sh
@@ -0,0 +1,6 @@
+rm -rf ./angular-first-look-hosted
+git clone https://github.com/johnpapa/angular-first-look-hosted.git
+cp bs-config.json ./angular-first-look-hosted
+cd angular-first-look-hosted
+npm install
+npm start
