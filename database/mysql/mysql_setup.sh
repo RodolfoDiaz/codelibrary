a4a142be10077d03890ca5ddd530cd0b276b5237
RodolfoDiaz <rodolfodc@hotmail.com>
2019-04-23 13:58:03 -0600

    Changed scripts

--- database/mysql/mysql_setup.sh
+++ database/mysql/mysql_setup.sh
@@ -1,15 +1,23 @@
 # https://hub.docker.com/_/mysql
-docker pull mysql
-docker image ls -a
-docker container ls -a
-docker ps
-# Starting a MySQL Server Instance
-docker run -p 3306:3306 --name mysql_container1 -e MYSQL_ROOT_PASSWORD=rootPwd1 -d mysql
-# start a bash session to the container
-docker exec -it mysql_container1 bash
-# start an SQL interactive console with root user
-docker exec -it mysql_container1 mysql -uroot -p
-# start an SQL interactive console with a test user
-docker exec -it mysql_container1 mysql --user=testUser -p
-docker start mysql_container1
-docker stop mysql_container1
+if [ "$1" == "up" ]; then
+    docker pull mysql
+    docker image ls -a
+    docker container ls -a
+    # Create a new MySQL container
+    docker run -p 3306:3306 --name mysql_container1 -e MYSQL_DATABASE=MyTestDB -e MYSQL_ROOT_PASSWORD=rootPwd1 -e MYSQL_USER=testUser -e MYSQL_PASSWORD=userPwd1 -h mySql1 -d mysql
+    echo 'Start the new container'
+    docker start mysql_container1
+elif [ "$1" == "down" ];  then
+    echo 'Stop and remove the container'
+    docker stop mysql_container1
+    docker rm mysql_container1
+elif [ "$1" == "sqlroot" ];  then
+    # start a SQL interactive console with root user
+    docker exec -it mysql_container1 mysql -uroot -p
+elif [ "$1" == "sqluser" ];  then
+    # start a SQL interactive console with a test user
+    docker exec -it mysql_container1 mysql --user=testUser -p
+elif [ "$1" == "bash" ];  then
+    # start a bash session to the container
+    docker exec -it mysql_container1 bash
+fi
--- database/mysql/test.sql
+++ database/mysql/test.sql
@@ -1,11 +1,12 @@
-create database MyClientDB;
-use MyClientDB;
+create database if not exists MyTestDB;
+grant select on MyTestDB.* to testUser;
+use MyTestDB;
 create table Client (
 	id int not null,
     first_name varchar(100),
     last_name varchar(100)
     );
     
-use MyClientDB;
+use MyTestDB;
 select * from Client;
 insert into Client values (1, 'Rodolfo', 'Diaz');
