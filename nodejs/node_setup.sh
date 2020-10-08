fa6c0514150860881e895ef1023dc54fe22b995e
RodolfoDiaz <rodolfo.a.diaz.cajina@intel.com>
2020-10-08 15:40:52 -0600

    Changed script

--- nodejs/node_setup.sh
+++ nodejs/node_setup.sh
@@ -2,14 +2,18 @@
 # https://nodejs.org/
 # Get .gitignore templates at this link: https://github.com/github/gitignore/blob/master/Node.gitignore
 if [ "$(uname)" == "Darwin" ]; then
-  brew install node
+    brew install node
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-  # Install Node Version Manager - https://github.com/nvm-sh/nvm
-  if ! [ -d ~/.nvm/ ]; then
-    wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
-  fi
-  # Install Node.js and select the latest version as default
-  source ~/.nvm/nvm.sh
-  nvm install 14 && nvm alias default 14
-  npm update -g
+    # Install Node.js using APT
+    # curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+    # sudo apt-get install -y nodejs
+    
+    # Install Node Version Manager - https://github.com/nvm-sh/nvm
+    if ! [ -d ~/.nvm/ ]; then
+        wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.36.0/install.sh | bash
+    fi
+    # Install Node.js and select the latest version as default
+    source ~/.nvm/nvm.sh
+    nvm install 14 && nvm alias default 14
+    npm update -g
 fi
