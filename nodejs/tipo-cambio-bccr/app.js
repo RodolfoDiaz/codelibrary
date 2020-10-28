922aa59a6bfbaa1382a87f48435e5f6108317fdd
RodolfoDiaz <rodolfo.a.diaz.cajina@intel.com>
2020-10-28 15:02:28 -0600

    Fixed typo

--- nodejs/tipo-cambio-bccr/app.js
+++ nodejs/tipo-cambio-bccr/app.js
@@ -1,16 +1,16 @@
 const tc = require('./tipo-cambio');
 // https://www.bccr.fi.cr/seccion-indicadores-economicos/servicio-web
-const correoElectroico = '';
+const correoElectronico = '';
 const tokenDeSuscripcion = '';
 
 // Obtener tipo de cambio de compra del dólar
 // para los últimos 30 dias (opción predefinida)
-tc.compra(correoElectroico, tokenDeSuscripcion).then((response) => {
+tc.compra(correoElectronico, tokenDeSuscripcion).then((response) => {
   console.log(response);
 });
 
 // Obtener tipo de cambio de venta del dólar
 // para los últimos 90 dias
-tc.venta(correoElectroico, tokenDeSuscripcion, 90).then((response) => {
+tc.venta(correoElectronico, tokenDeSuscripcion, 90).then((response) => {
   console.log(response);
 });
