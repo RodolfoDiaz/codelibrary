ce00db8ff740efd00865c511e4a624e17571004f
RodolfoDiaz <rodolfodc@hotmail.com>
2021-01-12 08:41:52 -0600

    Added new sample

---
+++ azure/vs/WatchPortalFunction/WatchInfo.cs
@@ -0,0 +1,36 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace WatchPortalFunction
+{
+    public static class WatchInfo
+    {
+        [FunctionName("WatchInfo")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            // Retrieve the model id from the query string
+            string model = req.Query["model"];
+
+            // If the user specified a model id, find the details of the model of watch
+            if (model != null)
+            {
+                // Use dummy data for this example
+                dynamic watchinfo = new { Manufacturer = "Abc", CaseType = "Solid", Bezel = "Titanium", Dial = "Roman", CaseFinish = "Silver", Jewels = 15 };
+
+                return (ActionResult)new OkObjectResult($"Watch Details: {watchinfo.Manufacturer}, {watchinfo.CaseType}, {watchinfo.Bezel}, {watchinfo.Dial}, {watchinfo.CaseFinish}, {watchinfo.Jewels}");
+            }
+            return new BadRequestObjectResult("Please provide a watch model in the query string");
+        }
+    }
+}
