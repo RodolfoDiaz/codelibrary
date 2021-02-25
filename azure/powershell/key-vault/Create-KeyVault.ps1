31a97fe2ea9cf48b7b7b986e68c8058b32aa4344
RodolfoDiaz <rodolfodc@hotmail.com>
2021-02-25 16:15:38 -0600

    Updated script

---
+++ azure/powershell/key-vault/Create-KeyVault.ps1
@@ -0,0 +1,94 @@
+# In StrictMode PowerShell generates a terminating error when the content of 
+# an expression or script-block violates basic best-practice coding rules.
+Set-StrictMode -Version latest
+# Exit immediately if a command exits with a non-zero status.
+$ErrorActionPreference = "Stop"
+
+# Azure Key Vault - https://docs.microsoft.com/en-us/azure/key-vault/general/
+# https://docs.microsoft.com/en-us/azure/key-vault/secrets/quick-create-powershell
+
+# The deployment process is:
+# 1- Log in to Azure.
+# 2- Create a resource group.
+# 3- Create a storage account.
+# 4- Get the storage account connection string.
+
+
+# --------------- 1 --------------- 
+Write-Host "---> Log in to Azure" -ForegroundColor Green
+# https://docs.microsoft.com/en-us/powershell/azure/authenticate-azureps
+Connect-AzAccount -UseDeviceAuthentication
+#$tenantId = (Get-AzContext).Tenant.Id
+
+Write-Host "---> Verify registration of the required Azure resource providers" -ForegroundColor Green
+# https://docs.microsoft.com/en-us/azure/azure-resource-manager/management/azure-services-resource-providers
+@("Microsoft.KeyVault", "Microsoft.Storage") | ForEach-Object {
+  Register-AzResourceProvider -ProviderNamespace $_
+}
+
+
+# --------------- 2 --------------- 
+Write-Host "---> Creating resource group" -ForegroundColor Green
+# https://docs.microsoft.com/en-us/powershell/module/az.resources/
+$rndResourceGroup = (New-Guid).ToString().Split("-")[0]
+$paramResourceGroup = "test_resourcegroup_$rndResourceGroup"
+$paramLocation = "westus"
+$paramTags = @{Environment = "Test"; Department = "IT" }
+
+$resourceGroup = Get-AzResourceGroup -Name "$paramResourceGroup" -ErrorAction SilentlyContinue
+if (-not $resourceGroup) {
+  # Create new Resource Group - Get-Help New-AzResourceGroup -Online
+  $resourceGroup = New-AzResourceGroup -Name "$paramResourceGroup" -Location "$paramLocation" -Tag $paramTags
+}
+Write-Host "---> Resource Group details:" -ForegroundColor Green
+$resourceGroup
+
+
+# --------------- 3 --------------- 
+Write-Host "---> Create a Key Vault" -ForegroundColor Green
+# Key Vault naming rule: length	3-24, Alphanumerics and hyphens.
+$rndKV = (New-Guid).ToString().Split("-")[0]
+$paramKeyVault = "test-keyvault-$rndKV"
+$ketVault = New-AzKeyVault -Name "$paramKeyVault" -ResourceGroupName "$paramResourceGroup" -Location "$paramLocation" -Tag $paramTags
+Write-Host "---> Key Vault details:" -ForegroundColor Green
+$ketVault
+
+
+# UNCOMMENT THIS SECTION AND SET USER PRINCIPAL NAME VALUE
+# Write-Host "---> Give a user account permissions to manage secrets in Key Vault" -ForegroundColor Green
+# $paramUserPrincipal = "user@domain.com" # take this value from Azure AD - Users -> User principal name
+# # See permissions lists here: https://docs.microsoft.com/en-us/powershell/module/az.keyvault/set-azkeyvaultaccesspolicy
+# Set-AzKeyVaultAccessPolicy -VaultName "$paramKeyVault" `
+#   -UserPrincipalName "$paramUserPrincipal"  `
+#   -PermissionsToSecrets get, set, delete `
+#   -PermissionsToCertificates list, get `
+#   -PermissionsToKeys list, get
+
+
+# --------------- 4 --------------- 
+Write-Host "---> Get a secret from the key vault" -ForegroundColor Green
+$paramSecretName1 = "SecretKey"
+#$paramSecretDesc = "This is a secret key"
+$paramSecretValue = ConvertTo-SecureString "Key-123$" -AsPlainText -Force
+$secretObject = Set-AzKeyVaultSecret -VaultName "$paramKeyVault" `
+  -Name "$paramSecretName1" -SecretValue $paramSecretValue -Tag $paramTags
+Write-Host "---> Secret details:" -ForegroundColor Green
+$secretObject
+
+
+# --------------- 5 --------------- 
+Write-Host "---> Get a secret from the key vault" -ForegroundColor Green
+$secret = Get-AzKeyVaultSecret -VaultName "$paramKeyVault" -Name "$paramSecretName1"
+$ssPtr = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($secret.SecretValue)
+try {
+  $secretValueText = [System.Runtime.InteropServices.Marshal]::PtrToStringBSTR($ssPtr)
+}
+finally {
+  [System.Runtime.InteropServices.Marshal]::ZeroFreeBSTR($ssPtr)
+}
+Write-Host "---> The secret key is $secretValueText" -ForegroundColor Magenta
+
+
+# Cleanup 
+# Remove Resource Group - Get-Help Remove-AzResourceGroup -Online
+# Get-AzResourceGroup -Name $paramResourceGroup | Remove-AzResourceGroup -Force
