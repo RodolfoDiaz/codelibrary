f57f70adadc9db9e8eed43d994d65a8a9213ec08
RodolfoDiaz <rodolfodc@hotmail.com>
2021-03-02 18:52:16 -0600

    Updated script

--- azure/azcli/sql/server-config.sh
+++ azure/azcli/sql/server-config.sh
@@ -1,9 +1,9 @@
-echo "Install SQL Server tools: sqlcmd and bcp"
+echo "Install sqlcmd and bcp the SQL Server command-line tools on Linux"
 # https://docs.microsoft.com/en-us/sql/linux/sql-server-linux-setup-tools?view=sql-server-ver15
+curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+curl https://packages.microsoft.com/config/ubuntu/18.04/prod.list | sudo tee /etc/apt/sources.list.d/msprod.list
+sudo apt-get update
+sudo ACCEPT_EULA=Y apt-get install -y mssql-tools unixodbc-dev
 echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bash_profile
 echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bashrc
 source ~/.bashrc
-curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
-curl https://packages.microsoft.com/config/ubuntu/16.04/prod.list | sudo tee /etc/apt/sources.list.d/msprod.list
-sudo apt-get update
-sudo ACCEPT_EULA=Y apt-get install -y mssql-tools unixodbc-dev
