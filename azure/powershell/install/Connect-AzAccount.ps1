b28e2f11833144f1f97651654f7eadaacb6ed314
RodolfoDiaz <rodolfodc@hotmail.com>
2021-04-16 14:41:47 -0600

    Updated script

--- azure/powershell/install/Connect-AzAccount.ps1
+++ azure/powershell/install/Connect-AzAccount.ps1
@@ -1,4 +1,9 @@
 # https://docs.microsoft.com/en-us/powershell/module/az.accounts/connect-azaccount
+# -UseDeviceAuthentication - Use device code authentication instead of a browser control.
 Connect-AzAccount
-Get-AzSubscription
+
+# List all users
 Get-AzADUser
+
+# Get subscriptions that the current account can access.
+Get-AzSubscription
