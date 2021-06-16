4a90fe3ef6059e6ed44cf42f12e9716ad170aec3
RodolfoDiaz <rodolfodc@hotmail.com>
2021-06-16 13:49:06 -0600

    Updated file

--- dotnet/projects/Cryptography/SecurityConsole/Program.cs
+++ dotnet/projects/Cryptography/SecurityConsole/Program.cs
@@ -1,106 +1,167 @@
 ﻿using CryptographyLibrary;
 using System;
+using System.IO;
 
 namespace SecurityConsole
 {
     class Program
     {
-        private const string plainText = "Hello World!";
+        private const string plainText = "Hello World";
         private const string password = "W3@kPa$sw0dSAr3N0Tg*oD";
         private static string salt = DateTime.UtcNow.ToString();
         private const string filePath = @"C:\Windows\win.ini";
 
         static void Main(string[] args)
         {
-            //HashTest();
-            //SymmetricEncryptionTest();
-            ShowCaesarSample();
+            if (args.Length == 0)
+            {
+                Console.WriteLine("No argurment found. Use 'C' for CaesarCipher, 'H' to show Input Text Hash, 'F' to show File Hash, 'E' for show Symmetric Encryption example.");
+            }
+            else
+            {
+                string argument = args[0].ToString().ToUpper();
+                if (argument == "C")
+                {
+                    ShowCaesarEncryption();
+                }
+                else if (argument == "H" || argument == "E")
+                {
+                    Console.Write("Enter a text (or hit Enter to continue with default): ");
+                    string inputText = Console.ReadLine();
+                    if (inputText == "")
+                    {
+                        inputText = plainText;
+                    }
+
+                    if (argument == "H")
+                    {
+                        HashTest(inputText);
+                    }
+                    else
+                    {
+                        SymmetricEncryptionTest(inputText);
+                    }
+                }
+                else if (argument == "F")
+                {
+                    Console.Write("Enter a file path (or hit Enter to continue with default): ");
+                    string inputFilePath = Console.ReadLine();
+                    if (inputFilePath == "")
+                    {
+                        inputFilePath = filePath;
+                    }
+                    else
+                    {
+                        if (File.Exists(inputFilePath))
+                        {
+                            FileHashTest(inputFilePath); 
+                        }
+                        else
+                        {
+                            Console.Write("File not found!");
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Argument is not valid.");
+                }
+            }
         }
 
-        private static void HashTest()
+        private static void HashTest(string inputText)
         {
-            ShowHashSample(Hash.ServiceProvider.MD5);
-            ShowHashSample(Hash.ServiceProvider.SHA1);
-            ShowHashSample(Hash.ServiceProvider.SHA256);
-            ShowHashSample(Hash.ServiceProvider.SHA384);
-            ShowHashSample(Hash.ServiceProvider.SHA512);
+            Console.WriteLine("--*-- Input Text Hash example --*--");
+            ShowInputTextHash(Hash.ServiceProvider.MD5, inputText);
+            ShowInputTextHash(Hash.ServiceProvider.SHA1, inputText);
+            ShowInputTextHash(Hash.ServiceProvider.SHA256, inputText);
+            ShowInputTextHash(Hash.ServiceProvider.SHA384, inputText);
+            ShowInputTextHash(Hash.ServiceProvider.SHA512, inputText);
         }
 
-        private static void SymmetricEncryptionTest()
+        private static void FileHashTest(string inputFilePath)
         {
-            ShowExample(SymmetricEncryption.ServiceProvider.AES);
-            ShowExample(SymmetricEncryption.ServiceProvider.DES);
-            ShowExample(SymmetricEncryption.ServiceProvider.RC2);
-            ShowExample(SymmetricEncryption.ServiceProvider.Rijndael);
-            ShowExample(SymmetricEncryption.ServiceProvider.TripleDES);
+            Console.WriteLine("--*-- File Hash example --*--");
+            ShowFileHash(Hash.ServiceProvider.MD5, inputFilePath);
+            ShowFileHash(Hash.ServiceProvider.SHA1, inputFilePath);
+            ShowFileHash(Hash.ServiceProvider.SHA256, inputFilePath);
+            ShowFileHash(Hash.ServiceProvider.SHA384, inputFilePath);
+            ShowFileHash(Hash.ServiceProvider.SHA512, inputFilePath);
         }
 
-        private static void ShowExample(SymmetricEncryption.ServiceProvider algorithm)
+        private static void SymmetricEncryptionTest(string inputText)
         {
-            string encryptedText = SymmetricEncryption.Encrypt(algorithm, plainText, password, salt);
+            Console.WriteLine("--*-- Symmetric Encryption example --*--");
+            ShowSymmetricEncryption(SymmetricEncryption.ServiceProvider.AES, inputText);
+            ShowSymmetricEncryption(SymmetricEncryption.ServiceProvider.DES, inputText);
+            ShowSymmetricEncryption(SymmetricEncryption.ServiceProvider.RC2, inputText);
+            ShowSymmetricEncryption(SymmetricEncryption.ServiceProvider.Rijndael, inputText);
+            ShowSymmetricEncryption(SymmetricEncryption.ServiceProvider.TripleDES, inputText);
+        }
+
+        private static void ShowSymmetricEncryption(SymmetricEncryption.ServiceProvider algorithm, string inputText)
+        {
+            string encryptedText = SymmetricEncryption.Encrypt(algorithm, inputText, password, salt);
             string decryptedText = SymmetricEncryption.Decrypt(algorithm, encryptedText, password, salt);
-            WriteToConsole(algorithm.ToString(), encryptedText, decryptedText);
-        }
 
-        private static void WriteToConsole(string algorithm, string encryptedText, string decryptedText)
-        {
             Console.WriteLine("---  Encryption Algorithm used {0}  ---", algorithm);
             Console.WriteLine("Encrypted Text:{0}", encryptedText);
             Console.WriteLine("Decrypted Text:{0}", decryptedText);
             Console.WriteLine();
         }
 
-        private static void ShowHashSample(Hash.ServiceProvider algorithm)
+        private static void ShowInputTextHash(Hash.ServiceProvider algorithm, string inputText)
         {
-            string hashValue = Hash.GetHash(algorithm, plainText);
-            bool hashMatches = Hash.VerifyHash(algorithm, plainText, hashValue);
-            string hashForFile = Hash.GetHashFromFile(algorithm, filePath);
-            WriteToConsoleHash(algorithm.ToString(), hashValue, hashMatches, hashForFile);
-        }
+            string hashValue = Hash.GetHash(algorithm, inputText);
+            bool hashMatches = Hash.VerifyHash(algorithm, inputText, hashValue);
 
-        private static void WriteToConsoleHash(string algorithm, string hash, bool hashMatches, string hashForFile)
-        {
-            Console.WriteLine("Generate the " + algorithm + " checksum of file: {0}", filePath);
-            Console.WriteLine(algorithm + " checksum is: {0}", hashForFile);
-            Console.WriteLine();
-
-            Console.WriteLine("The " + algorithm + " hash of " + plainText + " is: " + hash + "");
             if (hashMatches)
             {
-                Console.WriteLine("The hashes are the same.");
+                Console.WriteLine("The {0} hash for the input text: '{1}' is:\n{2}", algorithm, inputText, hashValue);
             }
             else
             {
-                Console.WriteLine("The hashes are not same.");
+                Console.WriteLine("Error!");
             }
             Console.WriteLine();
         }
 
-        private static void ShowCaesarSample()
+        private static void ShowFileHash(Hash.ServiceProvider algorithm, string inputFilePath)
         {
-            Console.WriteLine("Type a string to encrypt:");
-            string UserString = Console.ReadLine();
+            string hashForFile = Hash.GetHashFromFile(algorithm, inputFilePath);
+            Console.WriteLine("The {0} checksum of file: {1} is:\n{2}", algorithm, inputFilePath, hashForFile);
+            Console.WriteLine("You can compare the output above with the following PowerShell command: \nGet-FileHash {0} -Algorithm {1} | Format-List", inputFilePath, algorithm);
+            Console.WriteLine();
+        }
 
-            Console.WriteLine("\n");
+        private static void ShowCaesarEncryption()
+        {
+            Console.WriteLine("--*-- Caesar Encryption example --*--");
+            Console.WriteLine("Type a string to encrypt (or hit Enter to continue with default): ");
+            string inputText = Console.ReadLine();
+            if (inputText == "")
+            {
+                inputText = plainText;
+            }
 
-            Console.WriteLine("Enter your Key (numberic value)");
-            int key = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("\n");
+            Console.WriteLine("Enter your Key value (numberic value)");
+            string key = Console.ReadLine();
+            int intKey = 0;
+            if (!Int32.TryParse(key, out intKey))
+            {
+                intKey = -1;
+            }
 
+            Console.WriteLine("Encrypted Data with Caesar cipher by {0} spaces", intKey);
 
-            Console.WriteLine("Encrypted Data");
-
-            string cipherText = CaesarCipher.Encipher(UserString, key);
+            string cipherText = CaesarCipher.Encipher(inputText, intKey);
             Console.WriteLine(cipherText);
             Console.Write("\n");
 
             Console.WriteLine("Decrypted Data:");
 
-            string t = CaesarCipher.Decipher(cipherText, key);
+            string t = CaesarCipher.Decipher(cipherText, intKey);
             Console.WriteLine(t);
-            Console.Write("\n");
-
-            Console.ReadKey();
         }
     }
 }
