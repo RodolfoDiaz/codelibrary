b72ff3c167dd34fca4ece42fa4f6506125bedd3e
RodolfoDiaz <{ID}+{username}@users.noreply.github.com>
2026-09-04 16:06:20 -0600

    Updated script

--- dotnet/projects/WebAppContainer/Run-WebAppContainer.ps1
+++ dotnet/projects/WebAppContainer/Run-WebAppContainer.ps1
@@ -1,32 +1,31 @@
 # This file copies the web application created under $AppName folder in a Docker container.
-# See Dockerfile for details on how the image is built.
+# See the 'Dockerfile' in this folder for more details.
 
 New-Variable -Name "AppName" -Visibility Public -Value "myWebApp"
 New-Variable -Name "ContainerName" -Visibility Public -Value "mywebapp-container"
 New-Variable -Name "ImageName" -Visibility Public -Value "mywebapp-image"
 
-Write-Output "---> Copy Docker files"
+Write-Host "---> Copy Docker files inside the $AppName folder" -ForegroundColor Green
 Copy-Item "Dockerfile" -Destination $AppName
 Copy-Item ".dockerignore" -Destination $AppName
 Set-Location -Path $AppName
 
-Write-Output "---> Build application image"
+Write-Host "---> Build application image" -ForegroundColor Green
 docker build -t $ImageName .
 
-Write-Output "---> docker rm: Remove the container $ContainerName"
+Write-Host "---> Stop and remove the container $ContainerName (if exists)" -ForegroundColor Yellow
+docker stop $ContainerName
 docker rm $ContainerName
 
-Write-Output "---> docker run: Run a command in a new container"
+Write-Host "---> docker run: Run a command in a new container" -ForegroundColor Green
 docker run -d -p 8081:8080 --name $ContainerName $ImageName
 
 Write-Host "---> Browse to: http://localhost:8081.  Hit Enter to stop the container and perform cleanup" -ForegroundColor Green
 Read-Host
-Write-Host "Stopping the container..." -ForegroundColor Red
+Write-Host "Stopping the container..." -ForegroundColor Yellow
 docker stop $ContainerName
-Write-Output "---> docker ps: List all containers"
+Write-Host "---> docker ps: List all containers" -ForegroundColor Green
 docker ps -a
-# Write-Output "---> docker start: Start one or more stopped containers"
-# docker start $ContainerName
 
 # Display the question in green and keep the cursor on the same line
 Write-Host "Do you want to remove the containers and images created for $AppName? (Y/N)" -ForegroundColor Green -NoNewline
@@ -36,14 +35,17 @@ $response = Read-Host
 
 # Check if the response matches 'y' (case-insensitive)
 if ($response -eq 'y') {
-    Write-Output "---> docker rm: Remove the container $ContainerName" -ForegroundColor Red
+    Write-Host "---> Remove the container: $ContainerName" -ForegroundColor Red
     docker rm $ContainerName
-    Write-Output "---> docker rm: Remove image $ImageName" -ForegroundColor Red
+    Write-Host "---> Remove the image: $ImageName" -ForegroundColor Red
     docker image rm $ImageName
-    Write-Output "---> docker builder prune: Remove build cache" -ForegroundColor Red
+    Write-Host "---> Cleanup docker builder cache" -ForegroundColor Red
     docker builder prune --force
 } else {
     Write-Host "Action cancelled by user." -ForegroundColor Yellow
 }
 
 Set-Location ..
+
+# Write-Host "---> If you want to restart the container without rebuilding the image, run the following command:" -ForegroundColor Green
+# docker start $ContainerName
