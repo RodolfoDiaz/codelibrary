d79f725cb52a4ce6e305e101b764b80cf7e2ba3d
RodolfoDiaz <{ID}+{username}@users.noreply.github.com>
2026-09-17 10:49:57 -0600

    Updated script

---
+++ dotnet/projects/AuthDemoApp/AuthDemoApp.Web/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AuthDemoApp.Web.ViewModels
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
